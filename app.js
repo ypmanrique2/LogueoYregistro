0f24aba7a1bebd6826abd6b62842f6387f82f47e
Yadin Paulo Manrique Marquez <ypmanrique2@misena.edu.co>
2025-01-01 11:53:55 -0500

    reemplazo de puerto 3000 por process.env.PORT -- 3000

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const mysql = require('mysql2/promise');
 const fs = require('fs');
 const cors = require('cors');
